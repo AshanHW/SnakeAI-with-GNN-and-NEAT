e4e07ff5a5e92f75e875b94748a54b783fcdb1dc
AshanHW <shanedeutschland@gmail.com>
2026-02-01 18:22:43 +0100

    Add config file and classes

---
+++ Config/Config.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeGame
+{
+    public class Config
+    {
+        public class AppConfig
+        {
+            public GameConfig Game { get; set; }
+            public AIParameters AIParameters { get; set; }
+            public FitnessConfig Fitness { get; set; }
+            public NeatConfig Neat { get; set; }
+        }
+
+        public class GameConfig
+        {
+            public int Rows { get; set; }
+            public int Cols { get; set; }
+            public int MaxStepsPerFood { get; set; }
+        }
+
+        public class AIParameters
+        {
+            public int Generations { get; set; }
+            public int PopulationSize { get; set; }
+            public double CompatibilityThreshold { get; set; }
+            public int VisualSpeed {  get; set; }
+        }
+
+        public class FitnessConfig
+        {
+            public double FoodReward { get; set; }
+            public double DistanceReward { get; set; }
+            public double DeathPenalty { get; set; }
+            public double EfficiencyBonus { get; set; }
+        }
+
+        public class NeatConfig
+        {
+            public double C1 { get; set; }
+            public double C2 { get; set; }
+            public double C3 { get; set; }
+
+            public double WeightMutationRate { get; set; }
+            public double AddConnectionRate { get; set; }
+            public double AddNodeRate { get; set; }
+        }
+    }
+}
---
+++ Config/ConfigLoader.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http.Json;
+using System.Text;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+using static SnakeGame.Config;
+
+namespace SnakeGame
+{
+    public static class ConfigLoader
+    {
+        public static AppConfig Load(string relativePath = "Config/config.json")
+        {
+            string baseDir = AppContext.BaseDirectory;
+            string fullPath = Path.Combine(baseDir, relativePath);
+            if (!File.Exists(fullPath))
+                throw new FileNotFoundException($"Config file not found: {fullPath}");
+
+            string json = File.ReadAllText(fullPath);
+            return JsonSerializer.Deserialize<AppConfig>(json)
+                   ?? throw new Exception("Failed to parse config.json");
+        }
+    }
+}
